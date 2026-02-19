a948769d543a68643032bc3d3ab5f9573836de52
Premanshuankit <ankit.premanshu@gmail.com>
2026-02-19 13:53:21 +0530

    taking scrap_types out from listing and placed in config

---
+++ config/scrapTypes.js
@@ -0,0 +1,5 @@
+const SCRAP_TYPES = ["iron", "steel", "tina", "aluminium", "copper", "pital", "bronze", "carton", "paper", "newspaper", 
+                "magazine", "plastic", "pet-bottle", "glass", "e-waste", "battery", "cable-wire", "motor", "fridge",
+                "ac", "washing-machine"]
+
+module.exports = SCRAP_TYPES
